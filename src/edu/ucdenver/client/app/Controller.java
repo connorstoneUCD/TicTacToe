92d5a918ce6b1b9dc0f5eb22aa859155cb9d896c
ckwk <connor.stone@gmail.com>
2021-11-04 13:49:49 -0600

    Added method and attribute definitions to Controller

--- src/edu/ucdenver/client/app/Controller.java
+++ src/edu/ucdenver/client/app/Controller.java
@@ -1,11 +1,58 @@
 package edu.ucdenver.client.app;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Controller {
+    public Button btn_LookForGame;
+    public Label lbl_PlayerID;
+    public Button btn_ConnectToServer;
+    public Button btn_TopLeft;
+    public Button btn_BotRight;
+    public Button btn_BotMid;
+    public Button btn_BotLeft;
+    public Button btn_MidRight;
+    public Button btn_Mid;
+    public Button btn_MidLeft;
+    public Button btn_TopRight;
+    public Button btn_TopMid;
+    public Button btn_Return;
+    public Label lbl_Match;
+
     public void lookForGame(ActionEvent actionEvent) {
     }
 
     public void connect(ActionEvent actionEvent) {
+    }
+
+    public void topLeftPressed(ActionEvent actionEvent) {
+    }
+
+    public void botRightPressed(ActionEvent actionEvent) {
+    }
+
+    public void botMidPressed(ActionEvent actionEvent) {
+    }
+
+    public void botLeftPressed(ActionEvent actionEvent) {
+    }
+
+    public void midRightPressed(ActionEvent actionEvent) {
+    }
+
+    public void midPressed(ActionEvent actionEvent) {
+    }
+
+    public void midLeftPressed(ActionEvent actionEvent) {
+    }
+
+    public void topRightPressed(ActionEvent actionEvent) {
+    }
+
+    public void topMidPressed(ActionEvent actionEvent) {
+    }
+
+    public void returnToLogin(ActionEvent actionEvent) {
     }
 }
